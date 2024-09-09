90f952158665565c44c71394daca1920f88e51c3
Hugo Colin Miranda <colin.hugo.89@gmail.com>
2024-09-08 18:35:46 -0600

    CRUD terminado

---
+++ recursos-humanos-spring/src/main/java/gm/rh/modelo/Empleado.java
@@ -0,0 +1,27 @@
+package gm.rh.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idEmpleado;
+
+    String nombre;
+    String departamento;
+    BigDecimal sueldo;
+
+}
---
+++ recursos-humanos-spring/src/main/java/gm/rh/servicio/EmpleadoServicio.java
@@ -0,0 +1,38 @@
+package gm.rh.servicio;
+
+import gm.rh.modelo.Empleado;
+import gm.rh.repositorio.EmpleadoRepositorio;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class EmpleadoServicio implements IEmpleadoServicio{
+
+    private final EmpleadoRepositorio empleadoRepositorio;
+
+    @Override
+    public List<Empleado> listarEmpleados() {
+        return empleadoRepositorio.findAll();
+    }
+
+    @Override
+    public Empleado buscarEmpleadoPorId(Integer idEmpleado) {
+        Empleado empleado = empleadoRepositorio.findById(idEmpleado).orElse(null);
+        return empleado;
+    }
+
+    @Override
+    public Empleado guardarEmpleado(Empleado empleado) {
+        return empleadoRepositorio.save(empleado);
+    }
+
+    @Override
+    public void eliminarEmpleado(Empleado empleado) {
+        empleadoRepositorio.delete(empleado);
+    }
+}
